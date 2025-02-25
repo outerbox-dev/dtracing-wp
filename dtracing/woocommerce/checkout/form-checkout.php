09b1fa392a8f81a06790e04a6b9eeeebf98e9c88
Jose Gomez <jgomez@administrators-MacBook-Pro-3.local>
2025-02-25 16:51:45 -0600

    pushing updated file

--- dtracing/woocommerce/checkout/form-checkout.php
+++ dtracing/woocommerce/checkout/form-checkout.php
@@ -3,16 +3,6 @@
  * Checkout Form
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-checkout.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -35,36 +25,38 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-		<div class="col2-set" id="customer_details">
-			<div class="col-1">
-				<?php do_action( 'woocommerce_checkout_billing' ); ?>
+		<!-- Two Column Layout Wrapper -->
+		<div class="checkout-columns">
+
+			<!-- Left Column: Billing & Shipping -->
+			<div class="checkout-left">
+				<div id="customer_details">
+					<div class="col-1">
+						<?php do_action( 'woocommerce_checkout_billing' ); ?>
+					</div>
+
+					<div class="col-2">
+						<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+					</div>
+				</div>
 			</div>
 
-			<div class="shipping-option mobile">
-				<div style="display:none;"> <?php var_dump('text'); ?> </div>
-				<?php wc_cart_totals_shipping_html(); ?>
+			<!-- Right Column: Order Summary -->
+			<div class="checkout-right">
+			<h3 id="order-heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+				<div id="order_review" class="woocommerce-checkout-review-order">
+					
+
+					<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+				</div>
 			</div>
 
-			<div class="col-2">
-				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-			</div>
 		</div>
-
-		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
+		<!-- End Two Column Layout -->
+		<div class="customer-details">
+			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+		</div>
 	<?php endif; ?>
-	
-	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-	
-	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-	
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
-
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
 
 </form>
 
